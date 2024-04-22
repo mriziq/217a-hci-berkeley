6ea23e07e1ad411c8547a6f1b24ee6689d745fd6
mriziq <amerkmriziq@gmail.com>
2024-04-21 23:20:17 -0700

    ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 "use client"
-import { Box } from "@chakra-ui/react";
+import { Box, Text, HStack } from "@chakra-ui/react";
 import Chat from "./components/chat";
+import Link from "next/link";
 
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>School of Information @ UC Berkeley</h1>
+    <>
+      <HStack w="100%" justifyContent="space-between" p={5}>
+        <Text textAlign={"left"}>School of Information @ UC Berkeley<br />
+        </Text>
+        <Link href="https://www.github.com/mriziq/217a-hci-berkeley"><span style={{
+          color: "blue.800",
+          fontWeight: "normal"
+        }}>Github</span></Link>
+      </HStack>
       <Box p={5}>
         <Chat />
       </Box>
-    </div>
+    </>
   );
 };
 
